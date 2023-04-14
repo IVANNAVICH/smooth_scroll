9a2ff88f0f6923a1dc45c331466402c35eda867a
ivanstupak <navi.kaputs@gmail.com>
2023-04-14 15:01:42 +0300

    70% done

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
+//////////////Blur bubble/////////////////
+
 const move = document.getElementById("move")
 document.body.onpointermove = event => {
     const { pageX, pageY } = event;
-
 
 move.animate ({
     left: `${pageX}px`,
@@ -9,6 +10,7 @@ move.animate ({
 }, {duration: 2000, fill: "forwards"})
 };
 
+////////////Swiper///////////////
 
 new Swiper('.swiper', {
             spaceBetween: 12,
@@ -21,5 +23,29 @@ new Swiper('.swiper', {
         });
     
 
+///////video/////////////
 
+const videoBtn = $('.video-btn');
+const videoBox = $('.video');
+
+$(document).unbind("click").on("click", ".video-btn", toggleView);
+$(document).on("click", ".video", togglePause);
+
+function toggleView(e) {
+    e.preventDefault();
+    window.setTimeout(function(){
+        videoBox.toggleClass("visible");
+    }, 200);
+}
+
+function togglePause(e) {
+    e.preventDefault();
+    videoBox.toggleClass("visible");
+    videoBox.click(function(){
+        $('#myiframe').each(function(){
+          const refresh = $(this).attr("src");
+          $(this).attr("src", refresh);
+        });
+      });
+}
 
