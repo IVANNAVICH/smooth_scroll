0d1b500efa71ed09b3c4df611997ff1e9ce252be
ivanstupak <navi.kaputs@gmail.com>
2023-04-26 21:52:04 +0300

    100%

--- src/js/app.js
+++ src/js/app.js
@@ -49,3 +49,31 @@ function togglePause(e) {
       });
 }
 
+//-----------------TILT----------------------
+
+if ($(window).width() > 991)
+{
+const tilt = document.querySelectorAll(".effect");
+
+VanillaTilt.init(tilt, {
+    reverse: true,
+    max: 15,
+    speed: 300,
+    // scale: 1.12,
+    glare: true,
+    reset: true,
+    perspective: 500,
+    transition: true,
+    "max-glare": 0.45,
+    "glare-prerender": false,
+    gyroscope: true,
+    gyroscopeMinAngleX: -45,
+    gyroscopeMaxAngleX: 45,
+    gyroscopeMinAngleY: -45,
+    gyroscopeMaxAngleY: 45,
+    initDesktop
+});
+
+}
+
+//------------paralax------------
